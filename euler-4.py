7f059c0a04a0d9248a2b2299490c20658f3e05e5
FaisalBrudite <faisal.n.brudite@gmail.com>
2024-03-21 14:02:15 +0530

    4th euler problem

---
+++ euler-4.py
@@ -0,0 +1,15 @@
+
+'''
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20 ?
+
+'''
+num = 2
+while True:
+    
+    if all(num % n == 0 for n in range(1, 20)):
+        print(num)
+        break
+    else:
+        num += 2
